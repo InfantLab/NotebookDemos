db127e234f0e864174f515624b4818ef119137e8
Caspar Addyman <infantologist@gmail.com>
2022-03-28 09:54:44 +0100

    small updates for 202

---
+++ rot13.r
@@ -0,0 +1,6 @@
+rotX <- function(ch,x) 
+{ #rotate each letter of a string ch by x letters thru the alphabet, as long as x<=13
+  old <- paste(letters, LETTERS, collapse="", sep="")
+  new <- paste(substr(old, 2*x+1, 26*2), substr(old, 1, 26), sep="")
+  chartr(old, new, ch)
+}
